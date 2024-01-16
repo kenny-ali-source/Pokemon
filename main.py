81e2e43dc719bc9965f9e6f379527cec953977f4
jean <jean@gmail.com>
2024-01-16 16:30:38 +0100

    Merge unrelated histories

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
-import random
+from typing import List
 
 class Combat:
-    def __init__(self, pokemon1, pokemon2):
-        self.pokemon1 = pokemon1
-        self.pokemon2 = pokemon2
-        self.en_cours = True
+    def __init__(self, pokemonJoueur, pokemonAdversaire):
+        self.pokemonJoueur = pokemonJoueur
+        self.pokemonAdversaire = pokemonAdversaire
 
-    def attaquer(self, attaquant, adversaire):
-        degats = random.randint(1, 10)
-        adversaire.subir_degats(degats)
-        print(f"{attaquant.nom} attaque {adversaire.nom} et inflige {degats} dégâts!")
+    def getPuissanceAttaqueAdversaire(self, typeAdversaire):
+        # Logique pour obtenir la puissance d'attaque de l'adversaire en fonction du type
+        # Vous devez implémenter cette logique en fonction de vos règles de jeu
+        pass
 
-    def tour_de_combat(self):
-        self.attaquer(self.pokemon1, self.pokemon2)
-        self.attaquer(self.pokemon2, self.pokemon1)
+    def enleverPointsDeVie(self, defenseAdversaire):
+        # Logique pour enlever des points de vie en fonction de la défense de l'adversaire
+        # Vous devez implémenter cette logique en fonction de vos règles de jeu
+        pass
 
-        if self.pokemon1.est_ko() or self.pokemon2.est_ko():
-            self.en_cours = False
-
-    def commencer_combat(self):
-        print("Le combat commence!")
-
-        while self.en_cours:
-            self.tour_de_combat()
-
-        if self.pokemon1.est_ko():
-            print(f"{self.pokemon1.nom} a perdu le combat.")
-        elif self.pokemon2.est_ko():
-            print(f"{self.pokemon2.nom} a perdu le combat.")
+    def determinerVainqueur(self):
+        if self.pokemonJoueur.pointsDeVie <= 0:
+            return f"{self.pokemonAdversaire.nom} remporte la victoire!"
+        elif self.pokemonAdversaire.pointsDeVie <= 0:
+            return f"{self.pokemonJoueur.nom} remporte la victoire!"
         else:
-            print("Match nul.")
+            return "Le combat se poursuit."
+
+    def enregistrerDansPokedex(self, pokedex):
+        # Logique pour enregistrer le Pokémon rencontré dans le Pokédex
+        # Vous devez implémenter cette logique en fonction de vos règles de jeu
+        pass
